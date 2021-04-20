39dcca4e84b01f828eba9f7d5f9d175b2e034da9
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-04-21 01:47:06 +0200

    feat: Añado las rutas para CRUD de Administración
    Mostrar lista de Administraciones y la creación de una Administración:
    - /administracion
    - /administracion/
    
    Mostrar los detalles de una Administración:
    - /administracion/{id}
    
    Editar los detalles de una Administración:
    - /administracion/edit/{id}
    
    Borrar un Administración:
    - /administracion/delete/{id}
    
    Además se añade la ruta para la vista "Acerca de"
    - /about

--- atlas/routes/web.php
+++ atlas/routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Mostramos información del proyecto
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+// Rutas para los elementos Administración
+Route::prefix('administracion')->group(function () {
+    Route::redirect('/', '/administracion', 301);
+
+    // Mostrar formulario para crear elemento
+    // Mostrar a continuación los elementos ya creados
+    Route::get('/', 'App\Http\Controllers\AdministracionController@index')->name('administracion.index');
+
+    // Crear un elemento
+    Route::post('/', 'App\Http\Controllers\AdministracionController@store')->name('administracion.store');
+
+    // Mostrar un elemento
+    Route::get('/{administracion}', 'App\Http\Controllers\AdministracionController@show')->name('administracion.show');
+
+    // Mostrar el formulario de edición de un elemento
+    Route::get('/edit/{administracion}', 'App\Http\Controllers\AdministracionController@edit')->name('administracion.edit');
+
+    // Guardar un elemento editado
+    Route::post('/edit/{administracion}', 'App\Http\Controllers\AdministracionController@update')->name('administracion.update');
+
+    // Borrar un elemento
+    Route::delete('/delete/{administracion}', 'App\Http\Controllers\AdministracionController@destroy')->name('administracion.delete');
+});
