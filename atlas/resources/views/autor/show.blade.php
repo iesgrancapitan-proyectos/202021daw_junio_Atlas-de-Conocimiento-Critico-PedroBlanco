2056ea00ab45986f449993db8ce5d54dcc856882
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-06 19:25:10 +0200

    feat: Implemento autenticación en Autor

--- atlas/resources/views/autor/show.blade.php
+++ atlas/resources/views/autor/show.blade.php
@@ -1,19 +1,22 @@
-@include('layout.cabecera')
-<h1>Detalles del Autor</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Detalle: ') }}{{$autor->apellidos}}, {{$autor->nombre}}
+        </h2>
+    </x-slot>
 
-<div>
-    <label for="">Nombre:</label>
-    <p>{{$autor->nombre}}</p>
-</div>
-<div>
-    <label for="">Apellidos:</label>
-    <p>{{$autor->apellidos}}</p>
-</div>
-<div>
-    <label for="">P&aacute;gina web:</label>
-    <p><a href="{{$autor->url}}" target="_blank">{{$autor->url}}</a></p>
-</div>
-<div>
-    <a href="{{route('autor.index')}}">Volver</a>
-</div>
-@include('layout.fin')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">URL:</h2>
+                    <br/>
+                    <p class="border font-semibold text-l text-gray-800 leading-tight">
+                        <a href="{{$autor->url}}" target="_blank">{{$autor->url}}</a></p>
+                    <br/>
+                    <a href="{{route('autor.index')}}">Volver</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
