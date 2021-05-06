3ce6af6e4f82c755790c2f29257eeffec6f835d6
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-07 01:06:43 +0200

    feat: Relleno la parte de Acerca de... y Dashboard

--- atlas/resources/views/about.blade.php
+++ atlas/resources/views/about.blade.php
@@ -1,15 +1,17 @@
-@include('layout.cabecera')
-@include('layout.menu_usuario')
-<div>
-    <h1>Acerca del Proyecto <em>Atlas de Conocimiento Crítico</em></h1>
-    <div>
-        <h2>Operaciones implementadas:</h2>
-        <ul>
-            <li><a href="/administracion">CRUD de Administraciones.</a></li>
-            <li><a href="/ambito">CRUD de &Aacute;mbitos.</a></li>
-            <li><a href="/estado">CRUD de Estados.</a></li>
-            <li><a href="/autor">CRUD de Autores.</a></li>
-        </ul>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Acerca del Proyecto Atlas de Conocimiento Crítico') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    El Proyecto <em>Atlas de Conocimiento Crítico</em> es un directorio geolocalizado de mapas de conocimiento crítico elaborados en el entorno del grupo "Identificación de Conocimiento Crítico", dentro del Programa "Embajadores del Conocimiento", que es parte de la estrategia de Gestión del Conocimiento desarrollada por el Instituto Andaluz de Administración Pública (IAAP), de la Junta de Andalucía.
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-@include('layout.fin')
+</x-app-layout>
