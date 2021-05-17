0884c2572eb9709fa4bfa4aa551886a06f9ef3cb
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-17 13:37:08 +0200

    fix: Arreglo error al mostrar detalles de Geo
    Fixes #8

--- atlas/resources/views/geo/show.blade.php
+++ atlas/resources/views/geo/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Detalle de Localización: ') }}{{$estado->nombre}}
+            {{ __('Detalle de Localización: ') }}{{$geo->nombre}}
         </h2>
     </x-slot>
 
