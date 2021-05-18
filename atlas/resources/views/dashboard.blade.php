7cbf76d570d6a053f3b89c3a68fc9a80a9db351a
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-18 20:50:13 +0200

    Revert "feat: Instalo Laravel JetStream con Livewire"
    This reverts commit 34af12e9d8d2653cc10e481c4008626c7e2f724f.

--- atlas/resources/views/dashboard.blade.php
+++ atlas/resources/views/dashboard.blade.php
@@ -7,8 +7,17 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">Operaciones implementadas:</h2>
+                    <ul>
+                        <li><a href="/administracion">CRUD de Administraciones.</a></li>
+                        <li><a href="/ambito">CRUD de &Aacute;mbitos.</a></li>
+                        <li><a href="/estado">CRUD de Estados.</a></li>
+                        <li><a href="/geo">CRUD de Localizaciones Geogr&aacute;ficas.</a></li>
+                        <li><a href="/autor">CRUD de Autores.</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
--- atlas/tailwind.config.js
+++ atlas/tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
@@ -22,5 +21,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms')],
 };
