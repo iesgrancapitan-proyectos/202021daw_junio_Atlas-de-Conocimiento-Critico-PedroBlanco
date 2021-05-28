bf3bfe0bad9db78f2496aafb571722720be1a813
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-05-28 23:27:19 +0200

    fix: Arreglo captura de click fuera de modal
    Antes, para cerrar una ventana modal había que pulsar sobre cancelar.
    Ahora también se puede cerrar al pulsar en la zona sombreada alrededor.

--- atlas/resources/views/livewire/generic/create.blade.php
+++ atlas/resources/views/livewire/generic/create.blade.php
@@ -2,7 +2,7 @@
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div class="fixed inset-0 transition-opacity">
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+            <div wire:click="closeModal()" class="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
         <!-- This element is to trick the browser into centering the modal contents. -->
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>?
