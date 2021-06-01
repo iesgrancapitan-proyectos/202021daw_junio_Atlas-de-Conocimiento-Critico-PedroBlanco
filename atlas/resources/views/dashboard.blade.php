61f902ac42336d4ffd9db04798fc857c811bb6b8
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-01 20:27:18 +0200

    feat: Configuro servicio de mapas y publico ejemplo

--- atlas/resources/views/dashboard.blade.php
+++ atlas/resources/views/dashboard.blade.php
@@ -8,7 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                {{-- <x-jet-welcome /> --}}
+                <x-mapbox theme="light-v10" class="rounded-lg shadow-lg h-96 mt-6" :markers="[[-4.84772405594904,37.9567116],[-3.49205561203722,37.9557275]]" />
             </div>
         </div>
     </div>
