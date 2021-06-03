af6733d58f4d25f8b69ecf4eaf08e6bedefd0f40
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-03 22:53:30 +0200

    feat: Configuro los Seeders en el archivo principal

--- atlas/database/seeders/DatabaseSeeder.php
+++ atlas/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call(AmbitoSeeder::class);
+        $this->call(AdministracionSeeder::class);
+        $this->call(EstadoSeeder::class);
     }
 }
