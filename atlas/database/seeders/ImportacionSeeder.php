1b4e3ad20f54988f1b7c693151e3d02e5ef70810
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-05 01:26:57 +0200

    feat: Termino Seeder de datos iniciales
    La aplicación de los Seeders es idempotente (por ahora).

--- atlas/database/seeders/ImportacionSeeder.php
+++ atlas/database/seeders/ImportacionSeeder.php
@@ -20,7 +20,8 @@ class ImportacionSeeder extends Seeder
      */
     public function run()
     {
-        $nombre_archivo_csv = 'data/2021-06-04.csv';
+        $directorio = '/var/www/atlas/database/seeders/data/';
+        $nombre_archivo_csv = '2021-06-04.csv';
         $archivo_csv = false;
         $cabecera = [];
         $linea = [];
@@ -41,19 +42,19 @@ class ImportacionSeeder extends Seeder
                 // ¿Columnas mínimas? ¿De qué entidades?
                 // ¿Array asociativo a partir del título del fichero?
                 // ('nombre','apellidos') de Autor lo consideramos clave única (en la importación sólo)
-        if ( $archivo_csv = fopen ( $nombre_archivo_csv, 'r' ) !== false ) {
+        if ( ( $archivo_csv = fopen ( $directorio.$nombre_archivo_csv, 'r' ) ) !== false ) {
             // Quitamos línea de cabecera
             if ( ( $cabecera = fgetcsv ( $archivo_csv, 1000, ',', '"' ) ) !== false ) {
                 // Para cada línea con datos
                 while ( ($linea = fgetcsv ( $archivo_csv, 2000, ',', '"' ) ) !== false ) {
                     // Extraemos Administracion - Upsert
-                    $temp_administracion = $this->upsert_administracion ( $linea[5] );
+                    $temp_administracion = $this->updateOrCreate_administracion ( $linea[5] );
 
                     // Extraemos Ambito - Upsert
-                    $temp_ambito = $this->upsert_ambito ( $linea[7] );
+                    $temp_ambito = $this->updateOrCreate_ambito ( $linea[7] );
 
                     // Extraemos Estado - Upsert
-                    $temp_estado = $this->upsert_estado ( $linea[6] );
+                    $temp_estado = $this->updateOrCreate_estado ( $linea[6] );
 
                     // Extraemos Autor - InsertOrUpdate
                         // No podemos hacer Upsert - No tenemos un campo único y la combinación ('nombre','apellidos') no es única
@@ -62,10 +63,10 @@ class ImportacionSeeder extends Seeder
                     $temp_autor = $this->insert_autor ( $linea[8], $linea[9] );
 
                     // Extraemos Geo - Upsert
-                    $temp_geo = $this->upsert_geo ( $linea[10], $linea[11], $linea[12] );
+                    $temp_geo = $this->updateOrCreate_geo ( $linea[10], $linea[11], $linea[12] );
 
                     // Extraemos Mapa - Upsert
-                    $temp_mapa = $this->upsert_mapa ( $linea[1], $linea[2], $linea[3], $linea[4],
+                    $temp_mapa = $this->updateOrCreate_mapa ( $linea[1], $linea[2], $linea[3], $linea[4],
                                     $temp_administracion->id, $temp_ambito->id, $temp_estado->id, $temp_autor->id, $temp_geo->id
                     );
 
@@ -77,34 +78,25 @@ class ImportacionSeeder extends Seeder
         }
     }
 
-    protected function upsert_administracion ( $nombre )
+    protected function updateOrCreate_administracion ( $nombre )
     {
-        return Administracion::upsert([ // Primero los campos 'fillable' que vamos a _upsertar_
+        return Administracion::updateOrCreate([
                 'nombre' => $nombre,
-            ],
-            ['nombre'], // Segundo el campo 'unique' por el que nos vamos a guiar en el upsert
-            ['nombre'] // Tercero los campos que deben ser actualizados si el registro ya existe (no tienen que ser todos)
-        );
+        ]);
     }
 
-    protected function upsert_administracion ( $nombre )
+    protected function updateOrCreate_estado ( $nombre )
     {
-        return Administracion::upsert([ // Primero los campos 'fillable' que vamos a _upsertar_
+        return Estado::updateOrCreate([
                 'nombre' => $nombre,
-            ],
-            ['nombre'], // Segundo el campo 'unique' por el que nos vamos a guiar en el upsert
-            ['nombre'] // Tercero los campos que deben ser actualizados si el registro ya existe (no tienen que ser todos)
-        );
+        ]);
     }
 
-    protected function upsert_ambito ( $nombre )
+    protected function updateOrCreate_ambito ( $nombre )
     {
-        return Ambito::upsert([ // Primero los campos 'fillable' que vamos a _upsertar_
+        return Ambito::updateOrCreate([
                 'nombre' => $nombre,
-            ],
-            ['nombre'], // Segundo el campo 'unique' por el que nos vamos a guiar en el upsert
-            ['nombre'] // Tercero los campos que deben ser actualizados si el registro ya existe (no tienen que ser todos)
-        );
+        ]);
     }
 
     protected function insert_autor ( $apellidos, $nombre )
@@ -115,22 +107,19 @@ class ImportacionSeeder extends Seeder
         ]);
     }
 
-    protected function upsert_geo ( $nombre, $longitud, $latitud )
+    protected function updateOrCreate_geo ( $nombre, $longitud, $latitud )
     {
-        return Geo::upsert([ // Primero los campos 'fillable' que vamos a _upsertar_
+        return Geo::updateOrCreate([
                 'nombre' => $nombre,
                 'longitud' => $longitud,
                 'latitud' => $latitud,
-            ],
-            ['nombre'], // Segundo el campo 'unique' por el que nos vamos a guiar en el upsert
-            ['nombre','longitud','latitud'] // Tercero los campos que deben ser actualizados si el registro ya existe (no tienen que ser todos)
-        );
+        ]);
     }
 
-    protected function upsert_mapa ( $nombre, $descripcion, $f_creacion, $f_actualizado,
+    protected function updateOrCreate_mapa ( $nombre, $descripcion, $f_creacion, $f_actualizado,
                                     $id_administracion, $id_ambito, $id_estado, $id_autor, $id_geo )
     {
-        $mapa = Mapa::upsert([ // Primero los campos 'fillable' que vamos a _upsertar_
+        $mapa = Mapa::updateOrCreate([
                 'nombre' => $nombre,
                 'descripcion' => $descripcion,
                 'f_creacion' => $f_creacion,
@@ -138,10 +127,7 @@ class ImportacionSeeder extends Seeder
                 'administracion_id' => $id_administracion,
                 'ambito_id' => $id_ambito,
                 'estado_id' => $id_estado,
-            ],
-            ['nombre'], // Segundo el campo 'unique' por el que nos vamos a guiar en el upsert
-            ['nombre','descripcion','f_creacion','f_actualizado','administracion_id','ambito_id','estado_id'] // Tercero los campos que deben ser actualizados si el registro ya existe (no tienen que ser todos)
-        );
+        ]);
 
         $mapa->autores()->sync($id_autor);
         $mapa->geo()->sync($id_geo);
