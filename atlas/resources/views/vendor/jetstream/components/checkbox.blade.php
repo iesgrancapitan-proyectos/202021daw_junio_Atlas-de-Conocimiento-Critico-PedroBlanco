4efc9ec507095189608ea6a734dd86d36a1c3ec1
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-14 02:24:19 +0200

    feat: Cambio colores por defecto
    De gama de indigo a gama de rosa.

--- atlas/resources/views/vendor/jetstream/components/checkbox.blade.php
+++ atlas/resources/views/vendor/jetstream/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-pink-500 shadow-sm focus:border-pink-300 focus:ring focus:ring-pink-300 focus:ring-opacity-50']) !!}>
