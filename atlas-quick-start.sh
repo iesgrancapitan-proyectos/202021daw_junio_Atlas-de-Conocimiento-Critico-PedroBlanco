afef74df1d04760b0ab2b524da17d3a4e3c3777b
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-15 14:36:38 +0200

    fix: Arreglo acceso a contenedores en despligue rápido
    Además añado cabeceras de información al script de despliegue rápido.

--- atlas-quick-start.sh
+++ atlas-quick-start.sh
@@ -9,11 +9,23 @@
 ### git clone --recurse-submodules --branch develop https://github.com/iesgrancapitan-proyectos/202021daw_junio_Atlas-de-Conocimiento-Critico-PedroBlanco.git atlas
 ### cd atlas
 
+echo
+echo "*****************************************************"
+echo "***** INICIO - Copiando archivos de configuración ***"
+echo "*****************************************************"
+echo
+
 # Tomamos la configuración por defecto
 # FIXME: Añadir chequeos para no sobrescribir los cambios ya realizados
 cp atlas/.env.example atlas/.env
 cp laradock-atlas-daw/.env.example laradock-atlas-daw/.env
 cp laradock-atlas-daw/nginx/sites/atlas.conf.example laradock-atlas-daw/nginx/sites/atlas.conf
+
+echo
+echo "*****************************************************"
+echo "***** Generando y arrancando contenedores Docker ****"
+echo "*****************************************************"
+echo
 
 # Generamos y levantamos los contenedores
 cd laradock-atlas-daw
@@ -25,12 +37,45 @@ cd ..
 set -a
 source atlas/.env
 source laradock-atlas-daw/.env
-envsubst < install/atlas-init.sql | docker exec -it atlas_mariadb mysql -u root --password=$MARIADB_ROOT_PASSWORD
+
+echo
+echo "*****************************************************"
+echo "***** Configurando Base de datos  *******************"
+echo "*****************************************************"
+echo
+
+envsubst < install/atlas-init.sql | docker exec -i atlas_mariadb_1 mysql -u root --password=$MARIADB_ROOT_PASSWORD
 
 # Configuramos la parte Laravel de la aplicación
-docker exec -w /var/www/atlas atlas_workspace composer install
-docker exec -w /var/www/atlas atlas_workspace npm install
-docker exec -w /var/www/atlas atlas_workspace npm run dev
-docker exec -w /var/www/atlas atlas_workspace php artisan key:generate
-docker exec -w /var/www/atlas atlas_workspace php artisan migrate --seed
-docker exec -w /var/www/atlas atlas_workspace php artisan config:cache
+echo
+echo "*****************************************************"
+echo "***** Instalando dependencias con Composer **********"
+echo "*****************************************************"
+echo
+
+docker exec -w /var/www/atlas atlas_workspace_1 composer install
+
+echo
+echo "*****************************************************"
+echo "***** Instalando dependencias con NPM ***************"
+echo "*****************************************************"
+echo
+
+docker exec -w /var/www/atlas atlas_workspace_1 npm install
+docker exec -w /var/www/atlas atlas_workspace_1 npm run dev
+
+echo
+echo "*****************************************************"
+echo "***** Configurando aplicación Laravel ***************"
+echo "*****************************************************"
+echo
+
+docker exec -w /var/www/atlas atlas_workspace_1 php artisan key:generate
+docker exec -w /var/www/atlas atlas_workspace_1 php artisan migrate --seed
+docker exec -w /var/www/atlas atlas_workspace_1 php artisan config:cache
+
+echo
+echo "*****************************************************"
+echo "***** FIN: Pruebe a acceder a http://localhost/ *****"
+echo "*****************************************************"
+echo
