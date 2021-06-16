1861705525252c0e81983ee7b96f86bb2a0d7e8b
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-16 21:12:56 +0200

    fix: Actualizo middleware Checkrole con roles correctos

--- atlas/app/Http/Middleware/CheckRole.php
+++ atlas/app/Http/Middleware/CheckRole.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Role;
+
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,19 +18,23 @@ class CheckRole
      */
     public function handle(Request $request, Closure $next, string $role)
     {
-        if ( ($role == 'super') && (auth()->user()->role()->first()->nombre != 'SuperAdministrador') ) {
+        if ( ($role == 'super') && (auth()->user()->role_id != Role::IS_SUPER) ) {
             abort( 403 );
         }
 
-        if ( ($role == 'admin') && (auth()->user()->role()->first()->nombre != 'Administrador') ) {
+        if ( ($role == 'admin') && (auth()->user()->role_id != Role::IS_ADMIN) ) {
             abort( 403 );
         }
 
-        if ( ($role == 'Editor') && (auth()->user()->role()->first()->nombre != 'Administrador') ) {
+        if ( ($role == 'site_editor') && (auth()->user()->role_id != Role::IS_SITE_EDITOR) ) {
             abort( 403 );
         }
 
-        if ( ($role == 'user') && (auth()->user()->role()->first()->nombre != 'Usuario autenticado') ) {
+        if ( ($role == 'map_editor') && (auth()->user()->role_id != Role::IS_MAP_EDITOR) ) {
+            abort( 403 );
+        }
+
+        if ( ($role == 'user') && (auth()->user()->role_id != Role::IS_USER) ) {
             abort( 403 );
         }
 
