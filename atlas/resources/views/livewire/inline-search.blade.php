bc6dfe897afddf53bb5fd6475594f75bf2637b69
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-18 01:27:03 +0200

    feat: Avanzo en la búsqueda en vistas de modelos
    Y sin errata?

--- atlas/resources/views/livewire/inline-search.blade.php
+++ atlas/resources/views/livewire/inline-search.blade.php
@@ -5,7 +5,7 @@
         <div class="block space-x-4 space-y-4">
             {{-- <x-jet-label for="search" value="{{ __('Buscar') }}" /> --}}
             <input class='cthulhu border-gray-300 focus:border-pink-300 focus:ring focus:ring-pink-300 focus:ring-opacity-50 rounded-md shadow-sm w-1/2' name="query" id="query" wire:model="query" type="search" placeholder="Buscar..." value="{{$initial_query}}"/>
-            <x-button wire:click="inline_search"><x-fluentui-search-12 class="h-6 w-6" /></x-button>
+            <x-jet-button wire:click="inline_search"><x-fluentui-search-12 class="h-6 w-6" /></x-jet-button>
         </div>
         <div class="block space-x-4 space-y-4">
             {{$initial_query}} / {{$query}}
