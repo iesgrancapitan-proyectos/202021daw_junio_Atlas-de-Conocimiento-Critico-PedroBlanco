ad6c56ee71796b4405fa4fbd9e031c2740674361
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-18 07:38:35 +0200

    fix: Cosas raras con el mapa

--- atlas/resources/views/livewire/search.blade.php
+++ atlas/resources/views/livewire/search.blade.php
@@ -5,7 +5,7 @@
 
         <div class="block space-x-4 space-y-4 flex">
             {{-- <x-jet-label for="search" value="{{ __('Buscar') }}" /> --}}
-            <input class='cthulhu flex-1 border-gray-300 focus:border-pink-300 focus:ring focus:ring-pink-300 focus:ring-opacity-50 rounded-md shadow-sm w-1/2' name="query" id="query" wire:model="query" type="search" placeholder="Buscar..." value="{{$query}}"/>
+            <input class='cthulhu flex-1 border-gray-300 focus:border-pink-300 focus:ring focus:ring-pink-300 focus:ring-opacity-50 rounded-md shadow-sm w-1/2' name="query" id="query" wire:model.defer="query" type="search" placeholder="Buscar..." value="{{$query}}"/>
         </div>
     </form>
     <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
