740bc002f4aa68241432f210bbf593ea142c2d75
Pedro Blanco Wasmer <9304344+PedroBlanco@users.noreply.github.com>
2021-06-18 06:43:33 +0200

    feat:  Maqueto barra de navegaciones
    Preparada para público y para usuarios.

--- atlas/resources/views/navigation-menu.blade.php
+++ atlas/resources/views/navigation-menu.blade.php
@@ -1,6 +1,70 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        @guest
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="flex-shrink-0 flex items-center">
+                    <a href="{{ route('/') }}">
+                        <x-jet-application-mark class="block h-9 w-auto" />
+                    </a>
+                </div>
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('/') }}" :active="request()->routeIs('/')">
+                        {{ __('Inicio') }}
+                    </x-jet-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('atlas') }}" :active="request()->routeIs('atlas')">
+                        {{ __('Atlas') }}
+                    </x-jet-nav-link>
+                </div>
+            </div>
+
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <!-- Settings Dropdown -->
+                <div class="ml-3 relative">
+                    <x-jet-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <span class="inline-flex rounded-md">
+                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                    {{ __('Visitante') }}
+
+                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </button>
+                            </span>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            @if (Route::has('login'))
+                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">{{ __('messages.LogIn') }}</a>
+                                <div class="border-t border-gray-100"></div>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">{{ __('messages.Register') }}</a>
+                                @endif
+                            </div>
+                            @endif
+                        </x-slot>
+                    </x-jet-dropdown>
+                </div>
+            </div>
+
+            <!-- Hamburger -->
+            <div class="-mr-2 flex items-center sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+        @else
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
@@ -9,7 +73,6 @@
                         <x-jet-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
-
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
@@ -121,10 +184,39 @@
                 </button>
             </div>
         </div>
+        @endguest
     </div>
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        @guest
+        <div class="pt-2 pb-2 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('/') }}" :active="request()->routeIs('/')">
+                {{ __('Inicio') }}
+            </x-jet-responsive-nav-link>
+        </div>
+        <div class="pt-1 pb-1 border-t border-gray-200">
+            <div class="mt-3 space-y-1">
+                <x-jet-responsive-nav-link href="{{ route('atlas') }}" :active="request()->routeIs('atlas')">
+                    {{ __('Atlas') }}
+                </x-jet-responsive-nav-link>
+            </div>
+        </div>
+        <div class="pt-1 pb-1 border-t border-gray-200">
+            <div class="flex items-center px-4">
+                @if (Route::has('login'))
+                    <x-jet-responsive-nav-link href="{{ route('login') }}">
+                        {{ __('messages.LogIn') }}
+                    </x-jet-responsive-nav-link>
+                    @if (Route::has('register'))
+                        <x-jet-responsive-nav-link href="{{ route('register') }}">
+                            {{ __('messages.Register') }}
+                        </x-jet-responsive-nav-link>
+                    @endif
+                @endif
+            </div>
+        </div>
+        @else
         <div class="pt-2 pb-2 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('messages.Dashboard') }}
@@ -160,8 +252,8 @@
                 <x-jet-responsive-nav-link href="{{ route('livewire.users') }}" :active="request()->routeIs('livewire.users')">
                     {{ __('messages.Users') }}
                 </x-jet-responsive-nav-link>
-            @endcan
-        </div>
+                @endcan
+            </div>
         </div>
 
         <!-- Responsive Settings Options -->
@@ -203,5 +295,6 @@
                 </form>
             </div>
         </div>
+        @endguest
     </div>
 </nav>
